e76e4fbfeb51116c485b71b724dffc24d368d904
John St John <johnthesaintjohn@gmail.com>
2016-12-07 11:01:26 -0800

    bumping version

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ config = {
     'url': 'http://grit-bio.org/',
     'download_url': 'http://grit-bio.org/git/',
     'author_email': 'npboley@gmail.com',
-    'version': '2.0.9',
+    'version': '2.0.10',
     'packages': ['grit', 
                  'grit.analyze', 
                  'grit.files', 
